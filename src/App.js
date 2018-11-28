244a3e94a7cc66dc6da44b578ead6064f3879d67
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-11-28 12:59:28 +0300

    adding some new animations for text values and percentages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import anime from 'animejs';
 
 class App extends Component {
+
+  state = {
+    counter: 1
+  }
+  
   componentDidMount() {
     anime({
       targets: '#animated',
-      translateX: [
-        { value: 100, duration: 1200 },
-        { value: 50, duration: 800 }
-      ],
+      translateX: 200,
       backgroundColor: '#FFF',
       duration: 4000,
       loop: true
@@ -18,9 +20,50 @@ class App extends Component {
     anime({
       targets: '#animated2',
       left: '80%',
+      round: 1,
       opacity: .8,
       backgroundColor: '#FFF',
     })
+
+    var obj = { charged: 1};
+    anime({
+      targets: obj,
+      charged: 100,
+      easing: 'linear',
+      update: function() {
+        var el = document.querySelector('#counter');
+        el.innerHTML = parseInt(obj.charged);
+      },
+    });
+
+    var obj2 = { charged: '0%' };
+    var basicTimeline = anime.timeline({loop: true});
+    basicTimeline
+      .add({
+        targets: obj2,
+        charged: 100,
+        easing: 'linear',
+        update: function() {
+          var el = document.querySelector('#counter2');
+          el.innerHTML = parseInt(obj2.charged);
+        },
+        duration: 4000
+      })
+      .add({
+        targets: obj2,
+        charged: 0,
+        easing: 'linear',
+        update: function() {
+          var el = document.querySelector('#counter2');
+          el.innerHTML = parseInt(obj2.charged);
+        },
+        duration: 4000
+      })
+      // .add({
+      //   targets: '#basicTimeline .triangle.el',
+      //   translateX: 250,
+      //   easing: 'easeOutExpo'
+      // });
   }
 
   render() {
@@ -30,6 +73,9 @@ class App extends Component {
           <div id="animated" style={{backgroundColor: '#43484e', height: 100, width: 100}} />
           <br />
           <div id="animated2" style={{backgroundColor: '#43484e', height: 100, width: 100}} />
+          <h1>Counter : <span id="counter"></span>%</h1>
+          <br/>
+          <h1>Counter : <span id="counter2"></span>%</h1>
         </header>
       </div>
     );
