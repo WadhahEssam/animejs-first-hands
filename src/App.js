97dacda8fdc11babf5a7f74edc7c1a44db770dcb
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-11-28 04:00:35 +0300

    setting up anime.js and adding two animations to two squares

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import anime from 'animejs';
 
 class App extends Component {
+  componentDidMount() {
+    anime({
+      targets: '#animated',
+      translateX: [
+        { value: 100, duration: 1200 },
+        { value: 0, duration: 800 }
+      ],
+      rotate: '1turn',
+      backgroundColor: '#FFF',
+      duration: 2000,
+      loop: true
+    })
+
+    anime({
+      targets: '#animated2',
+      translateX: [
+        { value: 5, duration: 1200 },
+        { value: 0, duration: 800 }
+      ],
+      rotate: '2turn',
+      backgroundColor: '#FFF',
+      duration: 5000,
+      loop: true
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div id="animated" style={{backgroundColor: '#43484e', height: 100, width: 100}} />
+          <br />
+          <div id="animated2" style={{backgroundColor: '#43484e', height: 100, width: 100}} />
         </header>
       </div>
     );
