97f352a6662c6c343a86598331fdfec9f135f424
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-11-29 20:11:31 +0300

    adding an svg image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import anime from 'animejs';
+import testSvg from './test-icon.svg';
 
 class App extends Component {
 
@@ -114,12 +115,29 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <svg width="150" height="150" style={{border: '1px solid white', marginBottom: 10}}>
           <svg width="500" height="500" style={{border: '1px solid white', marginBottom: 10}}>
-          <rect x="100" y="250" rx="20" ry="20" width="150" height="150" />
+          <rect x="100" y="250" rx="20" ry="20" strokeWidth="100" width="150" height="150" />
             <circle cx="75" cy="75" r="50" stroke="#8C5650" strokeWidth="3" fill="#C2776F" />
             Sorry, your browser does not support inline SVG.
           </svg> 
+
+          {/* <img src={testSvg} className="test-svg" alt="testSvg" /> */}
+          <svg id='Layer_1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1366 768' fill="#91494E">
+            <polygon className='st0' points='437,460.5 437,265.4 606,167.8 775,265.4 775,460.5 606,558.1 700'
+            />
+            <path className='st1' d='M606,171.3l166,95.8v191.7l-166,95.8l-166-95.8V267.1L606,171.3 M606,164.4l-172,99.3v198.6l172,99.3l172-99.3 V263.7L606,164.4L606,164.4z'
+            />
+            <g>
+              <path className='st2' d='M353.3,489.9' />
+            </g>
+          </svg>
+
+          <div id="svgAttributes"> 
+            <svg width="128" height="128" viewBox="0 0 128 128">
+              <polygon points="64 69.03970505015754 8.574 99.97306569742874 63.07651523732741 67.43974602106434 64 3.973065697428737 64.92348476267259 67.43974602106434 119.426 99.97306569742874 " fill="currentColor"></polygon>
+            </svg> 
+          </div>
+
           <div id="animated" style={{backgroundColor: '#43484e', height: 100, width: 100}} />
           <br />
           <div id="animated2" style={{backgroundColor: '#43484e', height: 100, width: 100, borderRadius: 10}} />
